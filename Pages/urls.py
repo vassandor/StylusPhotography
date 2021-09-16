d94a8bf45522296ac415b5bbf19912fa57fe0939
Sandor Vas <snvas15@gmail.com>
2021-09-17 00:51:11 +0200

    update

--- Pages/urls.py
+++ Pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, gallery_page, about_page, contact_page
 
 urlpatterns = [
     path('', home_page),
+    path('gallery/', gallery_page),
+    path('about/', about_page),
+    path('contatct/', contact_page),
 ]
--- Pages/views.py
+++ Pages/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render, HttpResponse
 
 def home_page(request):
     return HttpResponse("<h1>Stylus Photography: Home</h1>")
+
+def contact_page(request):
+    return HttpResponse("<h1>Stylus Photography: Contact</h1>")
+
+def about_page(request):
+    return HttpResponse("<h1>Stylus Photography: About</h1>")
+
+def gallery_page(request):
+    return HttpResponse("<h1>Stylus Photography: Gallery</h1>")
