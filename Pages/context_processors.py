75c76b7e0d62c399da3238945fe47e1c01839740
Sandor Vas <snvas15@gmail.com>
2021-09-18 23:28:21 +0200

    update

---
+++ Pages/context_processors.py
@@ -0,0 +1,9 @@
+def global_context(request):
+    return {
+        "page_title": "Stylus Photography",
+        "subtitle": "Ha már unod az egyforma képeket...",
+        "phone": "+36-70-369-6077",
+        "email": "szmatefy@gmail.com",
+        "instagram": "https://www.instagram.com/stylus.photo/",
+        "facebook": "https://www.facebook.com/MatefyPhoto/"
+    }
