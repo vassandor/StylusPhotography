172460af4e4421a0a7672fc6fc0407ace8f0c684
Sandor Vas <snvas15@gmail.com>
2021-10-05 00:28:17 +0200

    update

--- Gallery/models.py
+++ Gallery/models.py
@@ -10,3 +10,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+class Photo(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    visible = models.BooleanField(default=False)
+    title = models.CharField(max_length=200)
+    image = models.ImageField()
+    description = models.TextField(max_length=2000)
+    uploaded = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=200, blank=True)
