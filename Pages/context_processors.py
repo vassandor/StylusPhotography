cfd73f437b631aff20f4af3515fc8f83f293a463
Sandor Vas <snvas15@gmail.com>
2021-10-04 23:19:29 +0200

    update

--- Pages/context_processors.py
+++ Pages/context_processors.py
@@ -1,11 +1,18 @@
+from .models import PageInfo
+
 def global_context(request):
+    page_info_list = PageInfo.objects.all()
+
+    if not page_info_list:
+        return {}
+
     return {
-        "page_title": "Stylus Photography",
-        "subtitle": "Ha már unod az egyforma képeket...",
-        "phone": "+36-70-369-6077",
-        "email": "szmatefy@gmail.com",
-        "instagram": "https://www.instagram.com/stylus.photo/",
-        "facebook": "https://www.facebook.com/stylusphoto/",
-        "company": "Visual Workshop Bt.",
-        "address": "Budapest, Hungary"
+        "page_title": page_info_list[0].title,
+        "subtitle": page_info_list[0].subtitle,
+        "phone": page_info_list[0].phone,
+        "email": page_info_list[0].email,
+        "instagram": page_info_list[0].instagram,
+        "facebook": page_info_list[0].facebook,
+        "company": page_info_list[0].company,
+        "address": page_info_list[0].address
     }
