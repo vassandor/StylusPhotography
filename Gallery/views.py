1fea440b3688f481def8f1a3cdc79830c06fabb1
Sandor Vas <snvas15@gmail.com>
2021-10-05 00:18:12 +0200

    update

--- Gallery/views.py
+++ Gallery/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 from django.views.generic import TemplateView, DetailView
 from utilities.data_gen import create_photo_list
+from .models import Category
 
 class GalleryView(TemplateView):
     template_name = "gallery.html"
@@ -8,6 +9,8 @@ class GalleryView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(GalleryView, self).get_context_data(**kwargs)
         context["images"] = create_photo_list(24)
+
+        context["categories"] = Category.objects.all()
         return context
 
 class PhotoDetailView(TemplateView):
