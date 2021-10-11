e5934f66edded68a060e2997025b6a17bf267332
Sandor Vas <snvas15@gmail.com>
2021-10-11 21:54:42 +0200

    update

--- Gallery/models.py
+++ Gallery/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.db.models.signals import pre_save
 
 # Create your models here.
 
@@ -20,7 +21,7 @@ class Photo(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     homepage = models.BooleanField(default=False)
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, blank=True)
+    slug = models.SlugField(max_length=200, blank=True, editable=False)
     image = models.ImageField(upload_to=image_dir_path)
     description = models.TextField(max_length=2000)
     uploaded = models.DateTimeField(auto_now_add=True)
@@ -30,3 +31,9 @@ class Photo(models.Model):
 
     def __str__(self):
         return self.title
+
+def slug_generator(sender, instance, **kwargs):
+    if not instance.slug:
+        instance.slug = slugify(instance.title)
+
+pre_save.connect(slug_generator, sender=Photo)
